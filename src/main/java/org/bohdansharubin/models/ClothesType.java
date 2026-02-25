9891e99affd4dbf72cd15e9e22b4bfb63f2a6bd7
Bohdan Sharubin <sharubin.reserve@gmail.com>
2026-02-25 17:01:09 +0200

    Added javadoc

--- src/main/java/org/bohdansharubin/models/ClothesType.java
+++ src/main/java/org/bohdansharubin/models/ClothesType.java
@@ -1,8 +1,19 @@
 package org.bohdansharubin.models;
 
+/**
+ * Represents different types of clothes.
+ */
 public enum ClothesType {
+
+    /** Headwear, such as hats or caps */
     HAT,
+
+    /** Lower body clothing, such as pants or trousers */
     PANTS,
+
+    /** Lower body clothing typically worn as a skirt */
     SKIRT,
-    SHIRT,
+
+    /** Upper body clothing, such as shirts */
+    SHIRT
 }
